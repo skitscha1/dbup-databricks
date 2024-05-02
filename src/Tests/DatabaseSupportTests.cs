ba9375201810c3e0f1862d30b3d257fdef00778d
skitscha1 <140205812+skitscha1@users.noreply.github.com>
2024-05-02 13:43:03 -0500

    Initial commit

---
+++ src/Tests/DatabaseSupportTests.cs
@@ -0,0 +1,20 @@
+﻿using DbUp.Builder;
+using DbUp.Tests.Common;
+
+namespace DbUp.NewProvider.Tests;
+
+public class DatabaseSupportTests : DatabaseSupportTestsBase
+{
+    public DatabaseSupportTests() : base()
+    {
+    }
+
+    protected override UpgradeEngineBuilder DeployTo(SupportedDatabases to)
+        => to.NewProviderDatabase("");
+
+    protected override UpgradeEngineBuilder AddCustomNamedJournalToBuilder(UpgradeEngineBuilder builder, string schema, string tableName)
+        => builder.JournalTo(
+            (connectionManagerFactory, logFactory)
+                => new NewProviderJournal(connectionManagerFactory, logFactory, tableName)
+        );
+}
---
+++ src/Tests/NoPublicApiChanges.cs
@@ -0,0 +1,11 @@
+﻿using DbUp.Tests.Common;
+
+namespace DbUp.NewProvider.Tests;
+
+public class NoPublicApiChanges : NoPublicApiChangesBase
+{
+    public NoPublicApiChanges()
+        : base(typeof(NewProviderExtensions).Assembly)
+    {
+    }
+}
